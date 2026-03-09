64373dbfadc0aaee59c79071f96e203520709c43
Tomas Klar <tomas.klar@allianz.com>
2026-03-09 15:59:30 +0100

    Add Atomic Tests

--- src/pages/projects_page.ts
+++ src/pages/projects_page.ts
@@ -1,4 +1,4 @@
-import { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 import { CreateNewProjectModal } from "./projects/create_new_project_modal";
 /***
 export class ProjectsPage {
@@ -20,13 +20,19 @@ export class ProjectsPage {
 export class ProjectsPage {
   readonly page: Page;
   readonly addProjectButton: Locator;
+  readonly projectsTable: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.addProjectButton = page.locator('[test_id="Add Project"]');
+    this.projectsTable = page.locator("#slimScroll table");
   }
 
   async clickAddProject() {
+    await expect(
+      this.projectsTable,
+      "Wait until Projects Table is visible",
+    ).toBeVisible();
     await this.addProjectButton.click();
     return new CreateNewProjectModal(this.page);
   }

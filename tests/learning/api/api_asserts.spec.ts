7a0f82f7752e0d586872760cec5e6486333aba74
Tomas Klar <tomas.klar@allianz.com>
2026-03-17 10:00:16 +0100

    API calls and asserts

--- tests/learning/api/api_asserts.spec.ts
+++ tests/learning/api/api_asserts.spec.ts
@@ -21,3 +21,27 @@ test("Assert Response Header", async ({ request }) => {
     "application/json; charset=utf-8",
   );
 });
+
+test("Response Body Assert", async ({ request }) => {
+  const response = await request.get(
+    "https://tegb-backend-877a0b063d29.herokuapp.com/train",
+  );
+  const responseBody = await response.json();
+  console.log(responseBody); // * Jen pro ukázku, v reálném testu bychom nepoužili
+
+  // * Kontroly (Asserty)
+  // ? Existence klíče v responseBody
+  expect(responseBody.id, "responseBody.id is defined").toBeDefined();
+  expect(responseBody, "responseBody have property message").toHaveProperty(
+    "message",
+  );
+
+  // ? Kontrola datového typu
+  expect(typeof responseBody.id, "responseBody.id is a number").toBe("number");
+
+
+  // ? Kontrola správnosti dat
+  expect(responseBody.message, "responseBody.message have text").toBe(
+    "TEG#B Training GET request successful",
+  );
+});
